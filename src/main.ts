e57ab7e5058d9175cfc448e0915544c54468d219
Zul Amri Durrin Nafis <zulamridn@Zuls-MacBook-Pro.local>
2024-10-01 09:34:08 +0700

    feat: add swegger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConsumerService } from './config/consumer.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+  .setTitle('Push Notification')
+  .setDescription(
+    'The API details of the business solution for the Push Notification Demo Application.',
+  )
+  .setVersion('1.0')
+  .addTag('Notification')
+  .addBearerAuth()
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT);
 }
--- src/notifications/dto/notification.dto.ts
+++ src/notifications/dto/notification.dto.ts
@@ -1,8 +1,25 @@
+import { ApiProperty, ApiBody } from '@nestjs/swagger';
 export class NotificationDto {
-    constructor(
-        public identifier: string,
-        public type: string,
-        public device: string,
-        public text: string
-    ){}
+    // constructor(
+    //     public identifier: string,
+    //     public type: string,
+    //     public device: string,
+    //     public text: string
+    // ){}
+
+    constructor(partial: Partial<NotificationDto>) {
+        Object.assign(this, partial);
+      }
+
+      @ApiProperty()
+      identifier: string;
+
+      @ApiProperty()
+      type: string;
+    
+      @ApiProperty()
+      device: string;
+
+      @ApiProperty()
+      public text: string
 }
